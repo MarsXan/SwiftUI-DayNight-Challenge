5d126bcb9b640500b3c184666cd0ff1feec5f656
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-09 18:19:39 +0330

    Initial Commit

---
+++ DayNightChallenge/DayNightChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayNightChallengeApp.swift
+//  DayNightChallenge
+//
+//  Created by mohsen mokhtari on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DayNightChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
