0545e3df6fa8a8650de5f7a4b4946f3507aaefe3
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-10 13:40:09 +0330

    update animations

--- DayNightChallenge/View/DayNightView.swift
+++ DayNightChallenge/View/DayNightView.swift
@@ -13,22 +13,31 @@ struct DayNightView: View {
     @State private var backgroundColor =  Color.a2_100
     @State private var leadingPadding:CGFloat = -150
     @State private var moonColor = Color.yellow
-        
         var simpleDrag: some Gesture {
             DragGesture()
                 .onChanged { value in
-                    if lineHeight < 150 {
-                        lineHeight += 2
+                    withAnimation(.spring(response: 0.4, dampingFraction: 0.3)){
+                        if lineHeight < 150 {
+                            lineHeight += 2
+                        }
                     }
                 }.onEnded{value in
-                    if lineHeight >= 130 {
-                        isNight.toggle()
-                        moonColor = isNight ? .white : .yellow
-                        backgroundColor = isNight ? .navyBlue : Color.a2_100
-                        leadingPadding = isNight ? -25 : -150
+                    withAnimation(.easeInOut){
+                        if lineHeight >= 130 {
+                            isNight.toggle()
+                            moonColor = isNight ? .white : .yellow
+                            backgroundColor = isNight ? .navyBlue : Color.a2_100
+                           
+                        }
+                        
+                        withAnimation(.easeInOut(duration: 0.7)){
+                            if lineHeight >= 130 {
+                                leadingPadding = isNight ? -25 : -150
+                            }
+                        }
+                        lineHeight  = 90
                     }
-                    
-                    lineHeight  = 90
+                   
                     
                 }
         }
@@ -46,24 +55,19 @@ struct DayNightView: View {
             VStack(spacing:0){
                 Circle()
                     .foregroundColor(moonColor)
-                    .animation(.easeInOut, value: moonColor)
                     .frame(width: 150,height: 150)
                     .overlay(alignment:.leading){
                         Circle()
                             .fill(backgroundColor)
                             .frame(width: 140,height: 140)
                             .padding(.leading,leadingPadding)
-                            
                             .padding(.bottom,25)
-                            .animation(.easeInOut, value: leadingPadding)
-                            
                     }
-                    .animation(.spring(response: 0.4, dampingFraction: 0.3), value: lineHeight)
                     
                 Rectangle()
                     .fill(moonColor)
                     .frame(width: 2,height: lineHeight)
-                    .animation(.spring(response: 0.4, dampingFraction: 0.3), value: lineHeight)
+                  
                     
                 Circle()
                     .fill(moonColor)
@@ -71,9 +75,6 @@ struct DayNightView: View {
                     .gesture(
                              simpleDrag.simultaneously(with: fingerDrag)
                             )
-                    .animation(.spring(response: 0.4, dampingFraction: 0.3), value: lineHeight)
-                    
-                    
                 
             }.vAlign(.top)
                 .offset(y:getRect().height/3)
